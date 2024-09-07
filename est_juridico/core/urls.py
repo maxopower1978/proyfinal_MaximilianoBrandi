91dbed4e81ffed534b3c642253dc2a99c300e65e
Maximiliano Brandi <maxi.brandi.2011@gmail.com>
2024-09-07 19:21:59 -0300

    feat: armado página inicial

---
+++ est_juridico/core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+from .views import CustomLoginView, CustomLogoutView
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(next_page='home'), name='logout'),
+]
--- est_juridico/core/views.py
+++ est_juridico/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
+def home(request):
+    return render(request, 'core/home.html')
+
+class CustomLoginView(LoginView):
+    template_name = 'login'
+    redirect_authenticated_user = True
+
+class CustomLogoutView(LogoutView):
+    next_page = 'logout'  # Redirige a 'home' después de cerrar sesión
